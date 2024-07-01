a91c52df14d5834f77f881b647a3f07b60404bf9
MisterCbing <da@susanin.news>
2024-07-01 11:30:44 +0400

    1.2

--- N and M.py
+++ N and M.py
@@ -3,14 +3,14 @@ n = int(input())
 def sum_dig(x):
     return sum(map(int, str(x)))
 
-min_del = n // 9 if n > 9 else 1
+min_del = int((n // 9 if n > 9 else 1) * '9')
 
-for i in range(min_del, min_del * 100_000):
+for i in range(min_del, min_del * 1000):
 
     if sum_dig(i * n) % n == 0:
         print(f'I got one\n{n * i}')
         break
 
-if i == min_del * 100_000 - 1:
+if i == min_del * 1000 - 1:
     print('suxx')
 
